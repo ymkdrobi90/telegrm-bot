72e9da1d2f75edd6bf830f318c3146c6ba6f3b3c
Mohammed1998 <ymkdrobi90i@gmail.com>
2026-06-20 09:38:07 +0300

    Update telegram_test.py

--- telegram_test.py
+++ telegram_test.py
@@ -6,9 +6,11 @@ app = Flask(__name__)
 TOKEN = "8838421364:AAEqo6oR47chV895sgDG78-HKJc5NoxLb-0"
 CHAT_ID = "957849521"
 
+
 @app.route("/", methods=["GET"])
 def home():
-    return "Bot is running"
+return "Bot is running"
+
 
 @app.route("/webhook", methods=["POST"])
 def webhook():
@@ -27,6 +29,7 @@ json={
 )
 
 return "OK", 200
+
 
 if __name__ == "__main__":
 app.run(host="0.0.0.0", port=10000)
