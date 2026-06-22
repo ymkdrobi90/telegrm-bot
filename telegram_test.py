9df15f31038d3db08431789cd62ad3a46f08c81d
Mohammed1998 <ymkdrobi90i@gmail.com>
2026-06-22 05:07:34 +0300

    Update telegram_test.py

--- telegram_test.py
+++ telegram_test.py
@@ -14,19 +14,19 @@ def home():
 def webhook():
     data = request.get_json(silent=True)
 
-    if data and "message" in data:
+if data and "message" in data:
     message = data["message"]
-    else:
+else:
     message = request.get_data(as_text=True) or "TradingView Alert"
 
-    url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
+url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
 
     requests.post(
     url,
     json={
-    "chat_id": CHAT_ID,
-    "text": message
-}
+        "chat_id": CHAT_ID,  
+        "text": message
+    }
 )
 
     return "OK", 200
