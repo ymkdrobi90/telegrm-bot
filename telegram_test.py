dcacb1d94c6bc67e9c0f69b2ffaa78e083889517
Mohammed1998 <ymkdrobi90i@gmail.com>
2026-06-22 06:13:38 +0300

    Update telegram_test.py

--- telegram_test.py
+++ telegram_test.py
@@ -21,14 +21,15 @@ def webhook():
 
     url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
 
-    requests.post(
-        url,
-        json={
-            "chat_id": CHAT_ID,
-            "text": message
-        }
-    )
+    response = requests.post(
+    url,
+    json={
+        "chat_id": CHAT_ID,
+        "text": message
+    }
+)
 
+print(response.text)
     return "OK", 200
 
 if __name__ == "__main__":
